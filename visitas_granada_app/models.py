9ffbffc063649412e75c48a3bbeda1a86a35c7ea
carlos-el <c-enriquez@outlook.com>
2020-03-16 15:47:20 +0100

    Añade campos fecha_pub y foto al modelo Visita

--- visitas_granada_app/models.py
+++ visitas_granada_app/models.py
@@ -4,7 +4,9 @@ class Visita(models.Model):
 	nombre      = models.CharField(max_length=100)
 	descripcion = models.CharField(max_length=1000)
 	likes       = models.IntegerField(default=0)
+	fecha_pub = models.DateTimeField('Fecha de publicación', auto_now_add=True)
+	foto = models.ImageField(default='fotos/default.png')
 
 class Comentario(models.Model):
-	visita      = models.ForeignKey(Visita, on_delete=models.CASCADE)
+	visita      = models.ForeignKey(Visita, on_delete=models.CASCADE, related_name='comentarios')
 	texto       = models.CharField(max_length=500)
