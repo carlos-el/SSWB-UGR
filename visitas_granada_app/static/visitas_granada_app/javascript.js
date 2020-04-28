97a5315a91c5445755d0931aafb411afdf03ba84
carlos-el <c-enriquez@outlook.com>
2020-04-28 04:40:06 +0200

    Añade modo oscuro y favicon

--- visitas_granada_app/static/visitas_granada_app/javascript.js
+++ visitas_granada_app/static/visitas_granada_app/javascript.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){ 
+
+    // Ejecutar este código cuando la página este caragada para poner el modo correcto
+    // TODO es posible colocar esto al inicio del head?
+    const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+    if(currentTheme) {
+        $(document.documentElement).attr("data-theme", currentTheme);
+    
+        if (currentTheme === 'dark') {
+            $("#theme-switcher-moon").css("display", "none")
+            $("#theme-switcher-sun").css("display", "inline-block")
+        }
+    }
+
     // Lanza confirmación hasta de enviar la petición de borrado de una visita
     // Se añade al evento click de los botones de borrado.
     $(".delVisitaButton").click(function(){ 
@@ -7,4 +20,23 @@ $(document).ready(function(){
             window.location.href = '/visita_borrar/' + $(this).data("id");
         } 
     }); 
+
+
+    // Funcion para alternar entre el modo ocuro y el claro
+    $("#theme-switcher-moon, #theme-switcher-sun").click(function(){ 
+        if($(document.documentElement).attr("data-theme") == "dark"){
+            $("#theme-switcher-moon").css("display", "inline-block")
+            $("#theme-switcher-sun").css("display", "none")
+            localStorage.setItem('theme', 'light');
+            $(document.documentElement).attr("data-theme", "light");
+        }else{
+            $("#theme-switcher-moon").css("display", "none")
+            $("#theme-switcher-sun").css("display", "inline-block")
+            localStorage.setItem('theme', 'dark');
+            $(document.documentElement).attr("data-theme", "dark");
+        }
+
+    }); 
+
+
 }); 
