ca1f05b7a5b2f5a9cc9d527c313b4344ee9af96e
carlos-el <c-enriquez@outlook.com>
2020-06-17 13:04:44 +0200

    Añade campos a Visita para localización geográfica

--- visitas_granada_app/models.py
+++ visitas_granada_app/models.py
@@ -1,18 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Visita(models.Model):
-	nombre      = models.CharField(max_length=100)
-	descripcion = models.CharField(max_length=1000)
-	likes       = models.IntegerField(default=0)
-	fecha_pub 	= models.DateTimeField('Fecha de publicación', auto_now_add=True)
-	foto 		= models.ImageField(default='fotos/default.png', upload_to = "fotos/")
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=1000)
+    likes = models.IntegerField(default=0)
+    fecha_pub = models.DateTimeField('Fecha de publicación', auto_now_add=True)
+    foto = models.ImageField(default='fotos/default.png', upload_to="fotos/")
+    lat = models.FloatField(default=37.196459)
+    lon = models.FloatField(default=-3.6261883)
+
 
 class Comentario(models.Model):
-	visita      = models.ForeignKey(Visita, on_delete=models.CASCADE, related_name='comentarios')
-	texto       = models.CharField(max_length=500)
-	# Fecha
-	# Usuario
+    visita = models.ForeignKey(
+        Visita, on_delete=models.CASCADE, related_name='comentarios')
+    texto = models.CharField(max_length=500)
+    # Fecha
+    # Usuario
 
 
 # Este modelo permite añadir características al usuario por defecto de Django pero
@@ -20,4 +25,3 @@ class Comentario(models.Model):
 class Perfil(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     token = models.CharField(max_length=512)
-	
